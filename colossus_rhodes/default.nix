0c0653b9d8258af5ca4618635a67cfca877155cf
nrs-status <sebaarsim@gmail.com>
2025-05-12 01:26:01 +0000

    exporting types and predicates

--- colossus_rhodes/default.nix
+++ colossus_rhodes/default.nix
@@ -3,4 +3,20 @@ rec {
   areIdenticalUnorderedLists = import ./areIdenticalUnorderedLists.nix { inherit prelib; };
   typecheck = import ./typecheck.nix { inherit prelib pkgslib; };
   tc = type: target: typecheck { inherit pkgslib prelib; } { inherit type target; };
+  predicates = prelib.importPairAttrsOfDir {
+    filePathForRecursiveFileListing = ./predicates;
+    inputsForImportPairs = {
+      lclInputs = {
+        inherit prelib pkgslib; 
+      };
+    };
+  };
+  types = prelib.importPairAttrsOfDir {
+    filePathForRecursiveFileListing = ./types;
+    inputsForImportPairs = {
+      lclInputs = {
+        inherit prelib pkgslib;
+      };
+    };
+  };
 }
